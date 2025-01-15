6a15de10132a06c466237d3f4d002171ae894c70
Israel Fertig <m583288718@gmail.com>
2025-01-15 17:10:33 +0200

    Add dotenv for environment variable management and update MongoDB connection string

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { connectToMongoDB } from "./config/DB.config.js";
+import { config } from "dotenv";
+config();
 connectToMongoDB();
 
 
 
-import express from 'express';
+import express from 'express'; 
 const app = express();
 
 import cors from 'cors';
--- config/DB.config.js
+++ config/DB.config.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 
 export const connectToMongoDB = () => {
-    const uri =`mongodb+srv://israelFertig:mf189010@cluster0.ut4rs.mongodb.net/Todo-list` 
-//   const connectionString = process.env.MONGO_CONNECTION;
+     
+   const connectionString = process.env.MONGO_CONNECTION;
   try {
-    mongoose.connect(uri);
+    mongoose.connect(connectionString);
     console.log("Mongo is here");
   } catch (error) {
-    console.log("Mongo is NOT here");
+    console.log("Mongo is NOT here");  
     console.log(error);
   }
 };
